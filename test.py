03add27e4e9e4dbe67ade121a202dcf3258a6ce5
ascv5 <46599708+ascv5@users.noreply.github.com>
2021-07-17 16:12:05 +0200

    movmnt avec clic droit terminé

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ import pygame
 pygame.font.init()
 
 #VAR
-X,Y = 1200, 600
+screen_w, screen_h = 1200, 600
+relative_x, relative_y, relative_w, relative_h = 0, 0, screen_w, screen_h
 objet = {}
 
 
 
-fenetre = pygame.display.set_mode((X, Y))
+fenetre = pygame.display.set_mode((screen_w, screen_h))
 
 img = pygame.image.load("test/2.png")
 img2 = pygame.image.load("test/1.jpg")
@@ -24,10 +25,15 @@ def add_objet(x, y, w, h, path):
 	objet[ide]["img"] = pygame.transform.scale(img, (w, h))
 
 
-def update_screen(x, y, size):
+def update_screen(x, y, w, h):
+	global relative_x, relative_y
+	print("X : " + str(relative_x) + " Y : " + str(relative_y))
+	relative_x, relative_y = x, y
+	fenetre.fill("black")
 	for a in objet.keys():
-		if x <= objet[a]["x"]+objet[a]["w"] <= x+size and y <= objet[a]["y"]+objet[a]["h"] <= y+size:
+		if x <= objet[a]["x"]+objet[a]["w"] <= x+w and y <= objet[a]["y"]+objet[a]["h"] <= y+h:
 			fenetre.blit(objet[a]["img"], (objet[a]["x"]-x, objet[a]["y"]-y))
+
 
 add_objet(100, 100, 200, 100, "test/2.png")
 add_objet(700, 500, 5, 5, "test/1.jpg")
@@ -59,7 +65,11 @@ while run:
 					update_screen(100, 100, 200)
 
 			if event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0] == True:
-				print("aX : " + str(pygame.mouse.get_pos()[0]-last_pos[0]) + " aY : " + str(pygame.mouse.get_pos()[1]-last_pos[1]))
+				ax = pygame.mouse.get_pos()[0]-last_pos[0]
+				ay = pygame.mouse.get_pos()[1]-last_pos[1]
+				print("aX : " + str(ax) + " aY : " + str(ay))
+				update_screen(relative_x-ax, relative_y-ay, relative_w, relative_h)
+
 
 	last_pos = pygame.mouse.get_pos()
 	pygame.display.update()
